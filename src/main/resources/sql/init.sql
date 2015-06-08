8772927109161731e8cbcaee7c7433d07bc37a0e
luciano <lucianohdr@gmail.com>
2015-06-07 23:24:03 -0300

    Categorias de questionarios padrao no banco

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -5,3 +5,8 @@ INSERT INTO tipousuario (id,descricao,nome,observacao) values (2, 'Coordenador d
 INSERT INTO tipousuario (id,descricao,nome,observacao) values (3, 'Chefe de Departamento', 'Chefe', 'Verifica resultados das avaliações dos cursos do departamento');
 INSERT INTO tipousuario (id,descricao,nome,observacao) values (4, 'Professor', 'Professor', 'Realiza auto-avaliação do professor, avaliação da turma, verifica resultados da avaliação da sua disciplina');
 INSERT INTO tipousuario (id,descricao,nome,observacao) values (4, 'Aluno', 'Aluno', 'Realiza auto-avaliação do aluno e avaliação do professor');
+
+INSERT INTO categoriaquestionario (id,descricao,nome ) values (1, 'Auto-avaliação do aluno', 'Auto-avaliação Aluno');
+INSERT INTO categoriaquestionario (id,descricao,nome ) values (2, 'Auto-avaliação do professor', 'Auto-avaliação professor');
+INSERT INTO categoriaquestionario (id,descricao,nome ) values (3, 'Avaliação da turma', 'Avaliação turma');
+INSERT INTO categoriaquestionario (id,descricao,nome ) values (4, 'Avaliação do professor', 'Avaliação professor');
