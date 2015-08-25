69d487511019b7a5c00532a7a8aae486362da4e4
luciano <lucianohdr@gmail.com>
2015-08-25 00:57:58 -0300

    adicionado controle de login no angular

---
+++ src/main/webapp/js/app/controller/menuController.js
@@ -0,0 +1,6 @@
+App.controller("MenuController", ['$scope', '$location', '$window', '$stateParams', '$state', 
+                                             
+     function($scope, $location, $window, $stateParams, $state){
+	
+	 }
+]);
